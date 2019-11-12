e5a4a18ce9936f74b91ca2c3028b5ef0f3571cdb
Chen Wang <cwxjtlu@gmail.com>
2019-11-11 18:25:50 -0600

    Add fake clint information

--- SQL/test.sql
+++ SQL/test.sql
@@ -15,3 +15,26 @@ SELECT * FROM update_keep_store(1, 1, 100);
 SELECT * FROM confirm_replenish_m2w(1);
 SELECT * FROM confirm_replenish_w2s(1);
 SELECT * FROM review_order(1, 5, 'It is Perfect!');
+--=====================Insert Fake Client=====================
+SELECT insert_online_client('Madeline','Hernandez','zhanson@hotmail.com','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Raymond','Mcdaniel','bcook@carter.info','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Philip','Foley','michael49@hester.com','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Robert','Wilson','tamisingh@hotmail.com','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Diane','Barker','williamsadriana@pearson.info','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Regina','Cruz','jessicahess@yahoo.com','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Tracy','Maxwell','ysmith@yahoo.com','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Kathy','Jones','andrewnorton@gmail.com','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Mary','Oliver','ymartinez@yahoo.com','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Alejandro','Watson','stacey47@rogers.info','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Justin','Leon','hardingjay@gmail.com','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Daniel','Meza','aimeemack@yahoo.com','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Joel','Foster','cummingsjuan@yahoo.com','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Timothy','Holt','lunajesse@flores-barnes.com','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Jennifer','Morgan','rogersstephanie@clark.net','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Mark','Morgan','josephtorres@snyder.com','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Michael','Chambers','tracey68@brown.com','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Travis','Wilson','natalieortiz@hotmail.com','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Brittany','Padilla','sanchezjessica@yahoo.com','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Sarah','Zamora','qjohnson@yahoo.com','MY-MASKED-PASSWORD');
+SELECT insert_online_client('Kathy','Mendez','davidmatthews@bailey-gordon.com','MY-MASKED-PASSWORD');
+--=====================Update Fake Pay=====================
