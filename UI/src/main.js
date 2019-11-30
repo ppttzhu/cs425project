41d26a0139d0288277a3ca2724e23c325eca599f
Yixin Peng <ypeng27@hawk.iit.edu>
2019-11-30 14:13:15 -0600

    Edit entry point

--- UI/src/main.js
+++ UI/src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import VueCookies from "vue-cookies";
 import Vuex from "vuex";
-import BootstrapVue from "bootstrap-vue";
 import axios from "axios";
 
 import App from "./App.vue";
@@ -10,17 +9,42 @@ import global from "./global.js";
 import { routes } from "./routes.js";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faTrash, faPencilAlt, faEye, faCheck , faTimes } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTrash,
+  faPencilAlt,
+  faEye,
+  faCheck,
+  faTimes
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  BFormInput,
+  BInputGroup,
+  BInputGroupAppend,
+  BInputGroupPrepend,
+  BButton,
+  BSpinner,
+  BContainer,
+  BRow,
+  BCol,
+} from "bootstrap-vue";
 import "./assets/styles/app.scss";
 
-library.add( faTrash, faPencilAlt, faEye, faCheck, faTimes );
+library.add(faTrash, faPencilAlt, faEye, faCheck, faTimes);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
+Vue.component("b-form-input", BFormInput);
+Vue.component("b-input-group", BInputGroup);
+Vue.component("b-input-group-append", BInputGroupAppend);
+Vue.component("b-input-group-prepend", BInputGroupPrepend);
+Vue.component("b-button", BButton);
+Vue.component("b-spinner", BSpinner);
+Vue.component("b-container", BContainer);
+Vue.component("b-row", BRow);
+Vue.component("b-col", BCol);
 
 Vue.use(VueRouter);
 Vue.use(VueCookies);
 Vue.use(Vuex);
-Vue.use(BootstrapVue);
 
 axios.defaults.baseURL =
   "https://3zo4ophla5.execute-api.us-east-1.amazonaws.com/cs425project_v1";
--- UI/vue.config.js
+++ UI/vue.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-outputDir: "../docs",
-publicPath: process.env.NODE_ENV === 'production'
-    ? '/cs425project/'
-    : '/'
+    outputDir: "../docs",
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/cs425project/'
+        : '/',
+    chainWebpack: config => {
+        config.performance
+          .maxEntrypointSize(400000)
+          .maxAssetSize(400000)
+      }
 };
